80f8a286bbc31530e04d7ddb0352b7018b085667
John Brestelli <jbrestell@gmail.com>
2017-12-11 20:10:25 +0000

    add string[] for scopes for a modelref;  use this for adding to the categories file
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EbrcModelCommon/trunk@85896 735e2a04-f8fc-0310-8a1b-f2942603c481

--- DatasetPresenter/src/main/java/org/apidb/apicommon/datasetPresenter/ModelReference.java
+++ DatasetPresenter/src/main/java/org/apidb/apicommon/datasetPresenter/ModelReference.java
@@ -1,38 +1,56 @@
 package org.apidb.apicommon.datasetPresenter;
 
 public class ModelReference {
-  private String recordClassName;
-  private String targetName;
-  private String targetType;
-  
-  public ModelReference(String recordClassName, String targetType, String targetName, String datasetName) {
-    this(targetType, targetName, datasetName);
-    this.recordClassName = recordClassName;
-    if (recordClassName == null) {
-      throw new UserException("Dataset " + datasetName + " contains a WDK model reference with a NULL record class name");
-    }
-  }
-  
-  public ModelReference(String targetType, String targetName, String datasetName) {
-    if (targetName == null) {
-      throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target name");
-    }
-    if (targetType == null) {
-      throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target type");
-    }
-    this.targetName = targetName;
-    this.targetType = targetType;
-  }
-  
-  String getRecordClassName() {
-    return recordClassName;
-  }
-  
-  String getTargetName() {
-    return targetName;
-  }
+    private String recordClassName;
+    private String targetName;
+    private String targetType;
+    private String[] scopes;
 
-  String getTargetType() {
-    return targetType;
-  }
+  
+    public ModelReference(String recordClassName, String targetType, String targetName, String datasetName, String[] scopes) {
+        this.recordClassName = recordClassName;
+        this.targetType = targetType;
+        this.targetName = targetName;
+        this.scopes = scopes;
+
+        if (recordClassName == null) {
+            throw new UserException("Dataset " + datasetName + " contains a WDK model reference with a NULL record class name");
+        }
+        if (targetName == null) {
+            throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target name");
+        }
+        if (targetType == null) {
+            throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target type");
+        }
+    }
+
+
+    public ModelReference(String targetType, String targetName, String datasetName, String[] scopes) {
+        this.targetName = targetName;
+        this.targetType = targetType;
+        this.scopes = scopes;
+        
+        if (targetName == null) {
+            throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target name");
+        }
+        if (targetType == null) {
+            throw new UserException("Dataset " + datasetName + " contains a model reference with a NULL target type");
+        }
+    }
+  
+    String getRecordClassName() {
+        return recordClassName;
+    }
+  
+    String getTargetName() {
+        return targetName;
+    }
+
+    String getTargetType() {
+        return targetType;
+    }
+
+    String[] getScopes() {
+        return scopes;
+    }
 }
