6a4b91f5504c9638705523279f631d57c663d80d
Dan Galdi <dmgaldi@upenn.edu>
2023-02-24 11:33:08 -0500

    Use project ID passed in instead of env var

--- Model/src/main/java/org/apidb/apicommon/model/userdataset/ISASimpleTypeHandler.java
+++ Model/src/main/java/org/apidb/apicommon/model/userdataset/ISASimpleTypeHandler.java
@@ -55,7 +55,7 @@ public class ISASimpleTypeHandler extends UserDatasetTypeHandler {
           .orElse("latest");
       String[] cmd = {"singularity", "run",
           "--bind", workingDir + ":/work",
-          "--bind", constructGusConfigBinding(),
+          "--bind", constructGusConfigBinding(projectId),
           "--bind", constructOracleHomeBinding(),
           "docker://veupathdb/dataset-installer-isasimple:" + imageTag,
           "loadStudy.bash",
@@ -85,7 +85,7 @@ public class ISASimpleTypeHandler extends UserDatasetTypeHandler {
         .filter(tagEnv -> !tagEnv.isBlank())
         .orElse("latest");
     String[] cmd = {"singularity", "run",
-        "--bind", constructGusConfigBinding(),
+        "--bind", constructGusConfigBinding(projectId),
         "--bind", constructOracleHomeBinding(),
         "docker://veupathdb/dataset-installer-isasimple:" + imageTag,
         "deleteStudy.pl",
@@ -93,9 +93,9 @@ public class ISASimpleTypeHandler extends UserDatasetTypeHandler {
     return cmd;
   }
 
-  private String constructGusConfigBinding() {
+  private String constructGusConfigBinding(String projectId) {
     return String.format("%s/config/%s/gus.config:/gusApp/gus_home/config/gus.config",
-        System.getenv("GUS_HOME"), System.getenv("PROJECT_ID"));
+        System.getenv("GUS_HOME"), projectId);
   }
 
   private String constructOracleHomeBinding() {
