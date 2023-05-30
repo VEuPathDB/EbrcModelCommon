85cc29f469f41b5c45e5c9a2b69ad37e83dc3054
Dan Galdi <dmgaldi@upenn.edu>
2023-05-30 16:42:22 -0400

    Revert "Revert to old BiomTypeHandler (#35)"
    This reverts commit 566726582786a2ab17b9334bcbb92d6792fdcf11.

--- Model/src/main/java/org/apidb/apicommon/model/userdataset/BiomTypeHandler.java
+++ Model/src/main/java/org/apidb/apicommon/model/userdataset/BiomTypeHandler.java
@@ -1,81 +1,117 @@
 package org.apidb.apicommon.model.userdataset;
 
-import java.nio.file.Path;
-import java.util.Map;
-import java.util.Set;
-
-import javax.sql.DataSource;
-
 import org.gusdb.wdk.model.WdkModelException;
-import org.gusdb.wdk.model.user.dataset.UserDataset;
-import org.gusdb.wdk.model.user.dataset.UserDatasetCompatibility;
-import org.gusdb.wdk.model.user.dataset.UserDatasetType;
-import org.gusdb.wdk.model.user.dataset.UserDatasetTypeFactory;
-import org.gusdb.wdk.model.user.dataset.UserDatasetTypeHandler;
+import org.gusdb.wdk.model.user.dataset.*;
 import org.json.JSONObject;
 
-public class BiomTypeHandler extends UserDatasetTypeHandler {
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-  private final static String NAME = "BIOM";
-  /* Can't actually handle all of those BIOM files
-   *  Only does 1.0 sparse files, with data points in the tsv file
-   *  This was done because there is no good way to add dependencies to the installer script
-   */
+public class BiomTypeHandler extends UserDatasetTypeHandler {
+  public final static String NAME = "BIOM";
   private final static String VERSION = "1.0, 2.0, or 2.1";
-  private final static String DISPLAY = "BIOM";
+  public final static String DISPLAY = "BIOM";
 
   @Override
-  public UserDatasetCompatibility getCompatibility(UserDataset userDataset, DataSource appDbDataSource) throws WdkModelException {
-    return new UserDatasetCompatibility(true, new JSONObject(), "");
+  public UserDatasetCompatibility getCompatibility(UserDataset userDataset, DataSource appDbDataSource) {
+    return new UserDatasetCompatibility(true, "");
   }
+
   @Override
   public UserDatasetType getUserDatasetType() {
     return UserDatasetTypeFactory.getUserDatasetType(NAME, VERSION);
   }
-
+  
   @Override
   public String getDisplay() {
-    return DISPLAY;
+	return DISPLAY;
   }
-  @Override
-  public String[] getInstallInAppDbCommand(UserDataset userDataset, Map<String, Path> fileNameToTempFileMap, String projectId, Path workingDir) {
-    Path metadataJson = fileNameToTempFileMap.get("metadata.json");
-    if (metadataJson == null) {
-      throw new RuntimeException("Failed to get: metadata.json");
-    }
-    Path dataTsv = fileNameToTempFileMap.get("data.tsv");
-    String dataTsvOrEmptyStringMeaningThingsToTheInstaller = dataTsv == null ? "" : dataTsv.toString();
 
-    String[] cmd = {
-        "installBiomUserDataset",
-        userDataset.getUserDatasetId().toString(),
-        metadataJson.toString(),
-        dataTsvOrEmptyStringMeaningThingsToTheInstaller,
-        projectId
-    };
-    return cmd;
+  @Override
+  public String[] getInstallInAppDbCommand(UserDataset userDataset,
+                                           Map<String, Path> fileNameToTempFileMap,
+                                           String projectId,
+                                           Path workingDir) {
+
+      Path metadataJson = fileNameToTempFileMap.get("metadata.json");
+      if (metadataJson == null) {
+          throw new RuntimeException("Failed to get: metadata.json");
+      }
+      Path dataTsv = fileNameToTempFileMap.get("data.tsv");
+      if (dataTsv == null) {
+          throw new RuntimeException("Failed to get: data.tsv");
+      }
+    try {
+      final UserDatasetMeta meta = userDataset.getMeta();
+      final JSONObject metaObject = new JSONObject();
+      final Path metaJsonTmpFile = Path.of(workingDir.toString(), "tmp-meta.json");
+      metaObject.put("name", meta.getName());
+      metaObject.put("description", meta.getDescription());
+      metaObject.put("summary", meta.getSummary());
+      Files.write(metaJsonTmpFile, metaObject.toString().getBytes(StandardCharsets.UTF_8));
+      final String imageTag = Optional.ofNullable(System.getenv("ISA_IMAGE_TAG"))
+          .filter(tagEnv -> !tagEnv.isBlank())
+          .orElse("latest");
+      String[] cmd = {"singularity", "run",
+          "--bind", workingDir + ":/work",
+          "--bind", constructGusConfigBinding(projectId),
+          "--bind", constructOracleHomeBinding(),
+          "docker://veupathdb/dataset-installer-isasimple:" + imageTag,
+          "loadStudyFromBiom.bash",
+          metadataJson.toString(),
+          dataTsv.toString(),
+          userDataset.getUserDatasetId().toString(),
+          metaJsonTmpFile.toString()};
+      return cmd;
+    } catch (WdkModelException | IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
   public Set<String> getInstallInAppDbFileNames(UserDataset userDataset) {
     try {
-      return userDataset.getFiles().keySet();
-    }
-    catch(WdkModelException e) {
-      throw new RuntimeException("Error Getting all files for this dataset: " + e.toString());
+      return userDataset.getFiles().values().stream()
+          .map(file -> file.getFileName())
+          .collect(Collectors.toSet());
+    } catch (Exception e) {
+      throw new RuntimeException(e);
     }
   }
 
   @Override
   public String[] getUninstallInAppDbCommand(Long userDatasetId, String projectId) {
-    String[] cmd = {"uninstallBiomUserDataset", userDatasetId.toString(), projectId};
+    final String imageTag = Optional.ofNullable(System.getenv("ISA_IMAGE_TAG"))
+        .filter(tagEnv -> !tagEnv.isBlank())
+        .orElse("latest");
+    String[] cmd = {"singularity", "run",
+        "--bind", constructGusConfigBinding(projectId),
+        "--bind", constructOracleHomeBinding(),
+        "docker://veupathdb/dataset-installer-isasimple:" + imageTag,
+        "deleteStudy.pl",
+        userDatasetId.toString()};
     return cmd;
+  }
+
+  private String constructGusConfigBinding(String projectId) {
+    return String.format("%s/config/%s/gus.config:/gusApp/gus_home/config/gus.config",
+        System.getenv("GUS_HOME"), projectId);
+  }
+
+  private String constructOracleHomeBinding() {
+    return System.getenv("ORACLE_HOME") + "/network/admin:/opt/oracle/instantclient_21_6/network/admin";
   }
 
   @Override
   public String[] getRelevantQuestionNames(UserDataset userDataset) {
-    String[] result = {"SampleQuestions.UserSampleByMetadata","SampleQuestions.UserSampleByTaxonAbundance"};
-    return result;
+    String[] empty = {};
+    return empty;
   }
-
 }
